81131281ffad7ceb2733c711485eb51dc2ef7495
Abin R <achuabinr11@gmail.com>
2023-12-12 11:58:39 +0530

    asgi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -187,8 +187,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 CORS_ALLOWED_ORIGINS = [
     "https://haven-frontend.vercel.app",
-    "wss://haven-frontend.vercel.app",
-    "https://haven.abinr.xyz",  # Add your backend domain if necessary
+    "ws://haven-frontend.vercel.app",
+    "https://haven.abinr.xyz",
+    "http://localhost:5173",  # Add your backend domain if necessary
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://haven-frontend.vercel.app', "https://haven.abinr.xyz",  'https://haven-frontend.vercel.app']
