cfba549cc245cf68ef0c33f30b42c365008fcbec
Abin R <achuabinr11@gmail.com>
2023-12-13 11:35:41 +0530

    activation templte

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -4,20 +4,19 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 # Setup Django before importing anything that depends on settings
-# backend/asgi.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django_application = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+# Now you can import other modules
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_application,
         "websocket": AuthMiddlewareStack(
             URLRouter(
-                chat.routing.websocket_urlpatterns
+                websocket_urlpatterns
             )
         ),
     }
 )
-
